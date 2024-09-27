2beed588dc8798cfdb49b71bb228d2b53e8e42c0
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2024-09-27 19:02:17 +0530

    winston-morgan

--- 02_express/index.js
+++ 02_express/index.js
@@ -1,8 +1,27 @@
 import 'dotenv/config'
 import express from 'express';
+import logger from "./logger.js";
+import morgan from "morgan";
 
+const morganFormat = ":method :url :status :response-time ms";
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 app.use(express.json())
 
@@ -14,11 +33,13 @@ app.post('/teas', (req, res) => {
   const { name, price } = req.body
   const newTea = { id: nextId++, name, price }
   teaData.push(newTea)
+  logger.info("This is an info message");
   res.status(200).send(newTea)
 })
 
 // viewing all teas
 app.get('/teas', (req, res) => {
+  logger.info("This is an info message");
   res.status(200).send(teaData)
 })
 
@@ -26,6 +47,7 @@ app.get('/teas', (req, res) => {
 app.get('/teas/:id', (req, res) => {
   const tea = teaData.find(t => t.id === parseInt(req.params.id))
   if (!tea) {
+    logger.error("This is an error message");
     return res.status(404).send('Tea Not Found!')
   }
   return res.status(200).send(tea)
@@ -35,6 +57,7 @@ app.get('/teas/:id', (req, res) => {
 app.put('/teas/:id', (req, res) => {
   const tea = teaData.find(t => t.id === parseInt(req.params.id))
   if (!tea) {
+    logger.error("This is an error message");
     return res.status(404).send('Tea Not Found!')
   }
   const { name, price } = req.body
@@ -47,6 +70,7 @@ app.put('/teas/:id', (req, res) => {
 app.delete('/teas/:id', (req, res) => {
   const index = teaData.findIndex(t => t.id === parseInt(req.params.id))
   if (index === -1) {
+    logger.error("This is an error message");
     return res.status(404).send("Not Found!")
   }
   teaData.splice(index, 1) // splice deleted for index to index+1 from the array
